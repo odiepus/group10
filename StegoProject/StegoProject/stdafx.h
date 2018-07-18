6a8d544f6562d503602a3230b2d43bc756e552b9
Travis80 <woods49warhorse@hotmail.com>
2018-07-18 18:48:40 -0500

    Add solution folder
    I fixed all the dependencies and issues that were caused by combining the files. It's a Win32 Console Application

---
+++ StegoProject/StegoProject/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1 
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1 
+#include "targetver.h"
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+#include <stdio.h>
+#include <io.h>
+#include <math.h>
+#include "BitmapReader.h"
+
+
+
+
+// TODO: reference additional headers your program requires here
